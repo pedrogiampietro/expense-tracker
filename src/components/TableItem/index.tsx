0bd62e5606124603e3f93db848e4b3098ee47ab9
Pedro Giampietro <pedro.giampietro@l8group.net>
2021-10-27 10:30:12 -0300

    create functions  to dateFilter.

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,5 +1,6 @@
 import * as C from './styles';
 import { IItem } from '../../types/IItem';
+import { formatDate } from '../../helpers/dateFilter';
 
 type Props = {
   item: IItem;
@@ -8,7 +9,7 @@ type Props = {
 export function TableItem({ item }: Props) {
   return (
     <C.TableLine>
-      <C.TableColumn>Data</C.TableColumn>
+      <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
       <C.TableColumn>{item.category}</C.TableColumn>
       <C.TableColumn>{item.title}</C.TableColumn>
       <C.TableColumn>R$ {item.value}</C.TableColumn>
